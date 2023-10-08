bdd7dc5537a4d8fde0684785d3c3d47cb18012b7
Ansh Thakur <mailtoanshthakur@gmail.com>
2023-10-08 10:17:10 +0530

    DONE

--- Test.java
+++ Test.java
@@ -1,17 +1,25 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test {
     /*
      * Test file for testing the code before applying it in the main program😁😁
      */
     public static void main(String[] args) {
-        String[] arr = { "Ansh", "Thakur", "OP" };
-        String[] reverseArray = new String[3];
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = 0; j < arr.length; j++) {
-                reverseArray[j] = arr[i];
+        int[] arr = { 21, 87, 64, 95, 37, 95, 24, 61, 58, 36, 97 };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the target elements :--> ");
+        int target = sc.nextInt();
+        int target_index = LinearSearch_Algo(arr, target);
+        System.out.println("The target element is present at the index " + target_index + ".");
+        sc.close();
+    }
+
+    static int LinearSearch_Algo(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) {
+                return i;
             }
         }
-        System.out.println(Arrays.toString(reverseArray));
+        return -1;
     }
 }
