f30b8bdfdf308325845c13ad7af7c98269d3ce2a
Ansh Thakur <mailtoanshthakur@gmail.com>
2023-10-09 10:54:00 +0530

    DONE

--- _REVISED/InsertionSort_Algo.java
+++ _REVISED/InsertionSort_Algo.java
@@ -1,16 +1,15 @@
-package SORTING.INSERTION_SORT;
+package _REVISED;
 
 import java.util.Arrays;
 
 public class InsertionSort_Algo {
     public static void main(String[] args) {
         int[] arr = { 15, 89, 789, 0, 15, 348, 952, 62 };
-
-        Insertionsort_Algo(arr);
+        InsertionSort_Algo_op(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void Insertionsort_Algo(int[] arr) {
+    static void InsertionSort_Algo_op(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
