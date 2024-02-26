91edceaa418dccb4d13310fd456d817009fdd18b
Ansh Thakur <mailtoanshthakur@gmail.com>
2024-02-26 22:30:37 +0530

    DONE

--- Lessons/Comments.java
+++ Lessons/Comments.java
@@ -1,19 +1,22 @@
 package Lessons;
+
 public class Comments {
-    
-    //There are basically three types of comments in java ==
-    // 1- Single line comments - 
-    
+
+    // There are basically three types of comments in java ==
+    // 1- Single line comments -
+
     // 2- Multiline comments -
-    /*Java programming is the best programming language
+    /*
+     * Java programming is the best programming language
      * in the entire world.
      */
 
     // 3- Documentation Comments -
-     /**
-     Java programming
-     tells us
-     about the
-     power of
-     leaning and having the knowledge of Coding in java. */
+    /**
+     * Java programming
+     * tells us
+     * about the
+     * power of
+     * leaning and having the knowledge of Coding in java.
+     */
 }
--- _REVISED/SelectionSort1.java
+++ _REVISED/SelectionSort1.java
@@ -2,10 +2,9 @@ package _REVISED;
 
 import java.util.Arrays;
 
-public class SelectionSort {
+public class SelectionSort1 {
     public static void main(String[] args) {
-        int[] arr = { 12, 87, 0, 41, 63
-        };
+        int[] arr = { 14, 55, 78, 1, 4, 0, 35, 41, 89 };
         Selection(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -14,15 +13,15 @@ public class SelectionSort {
         int max_index = 0;
         for (int i = 0; i < arr.length; i++) {
             max_index = arr.length - 1 - i;
-            int max_ele_index = getMax_Element(arr, max_index);
+            int max_ele_index = getMax_Element_index(arr, 0, max_index);
             swap(arr, max_index, max_ele_index);
         }
 
     }
 
-    static int getMax_Element(int[] arr, int max_index) {
+    static int getMax_Element_index(int[] arr, int start, int max_index) {
         int max_ele_index = 0;
-        for (int i = 0; i < max_index; i++) {
+        for (int i = 0; i < start; i++) {
             if (arr[max_ele_index] < arr[i]) {
                 max_ele_index = i;
             }
@@ -31,8 +30,9 @@ public class SelectionSort {
     }
 
     static void swap(int[] arr, int max_index, int max_ele_index) {
-        int tempp = arr[max_index];
+        int temp = max_index;
         arr[max_index] = arr[max_ele_index];
-        arr[max_ele_index] = tempp;
+        arr[max_ele_index] = temp;
     }
+
 }
